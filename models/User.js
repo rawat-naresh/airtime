cc477a5334d05790b84cc21676c7fa61f156e412
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-03 12:51:44 +0530

    UserSchema Created

--- models/User.js
+++ models/User.js
@@ -1,20 +1,27 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-let jsonwebtoken = require('jsonwebtoken');
-let secret = require('../config');
 
 
 /* Defining User schema  */
 
 let UserSchema = new Schema({
+    firstname:{type: String, required:[true, 'is required']},
+    lastname:{type: String, required:[true, 'is required']},
+    username:{type: String, required:[true, 'is required'],unique:true, match: [/^[a-zA-Z0-9_]+$/, 'is invalid'], index: true},
+    bio:{type: String, maxlength:100},
+    birthday:{type: Date},
+    relationship:{type: String, enum:['Single','Married','Occupied','Complicated']},
+    contact:{type: String, match: [/^[0-9]+$/, 'is invalid']},
+    profile:String,
+    wall:String,
+    followers:[{type: Schema.Types.ObjectId, ref: ''}],
+    following:[{type: Schema.Types.ObjectId,ref:''}],
+    tweets:[{type: Schema.Types.ObjectId,ref:''}],
+    likedTweets:[{type: Schema.Types.ObjectId, ref:''}],
+    sharedTweets:[{type: Schema.Types.ObjectId,ref:''}],
+    mentions:[{type: Schema.Types.ObjectId, ref:''}]
 
 });
 
-UserSchema.methods.toAuthJSON = function() {
-    return {
-        username:this.username,
-        token:this.generateJWT,
-    };
-}
 
 module.exports = mongoose.model('user',UserSchema);
