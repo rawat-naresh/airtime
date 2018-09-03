fc0de76b4f4e0da71782622521433ca62dd498e7
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-03 18:34:08 +0530

    mongoose validation handler added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,22 @@
 let router = require('express').Router();
 router.use('/', require('./users'));
+
+
+
+/* Mongoose validation error will be handelled here */
+router.use(function(err, req, res, next){
+    if(err.name === 'ValidationError'){
+      return res.status(422).json({
+        errors: Object.keys(err.errors).reduce(function(errors, key){
+          errors[key] = err.errors[key].message;
+  
+          return errors;
+        }, {})
+      });
+    }
+  
+    return next(err);
+});
+
+
 module.exports = router;
