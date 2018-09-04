ca544662f4933e7048e3a698f8f93095ef2c982c
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-04 12:21:04 +0530

    new route file created. it contains routes for settings

---
+++ routes/api/settings.js
@@ -0,0 +1,39 @@
+let router = require('express').Router();
+let User = require('../../models/User');
+let UserCredential = require('../../models/UserCredential');
+
+router.get('/account', function(req, res, next) {
+
+});
+router.put('/account', function(req, res, next) {
+
+})
+router.get('/password', function(req, res, next) {
+
+});
+router.put('/password', function(req, res, next) {
+
+})
+router.get('/contact', function(req, res, next) {
+
+});
+router.put('/contact', function(req, res, next) {
+
+});
+
+router.put('/profile-pic', function(req, res, next) {
+
+});
+
+router.put('/profile-wall', function(req, res, next) {
+
+});
+router.delete('/profile-pic', function(req, res, next) {
+
+});
+
+router.delete('/profile-wall', function(req, res, next) {
+
+});
+
+module.exports = router;
